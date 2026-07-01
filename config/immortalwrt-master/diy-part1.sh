47c0888329534ebe8e8161ae5ac3c8f97974f21d
Sun2ray <keshunhuan@gmail.com>
2026-07-01 23:20:51 +0800

    Add Argon theme and configuration app
    Added Argon theme source and configuration app to the build.

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -13,4 +13,7 @@
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 # 添加 PassWall2 官方源
 echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >> feeds.conf.default
+# 添加 Argon 主题源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
